956a313c8af727609d5bbdd3e8c81a8707190bf0
Heitor147 <54001761+Heitor147@users.noreply.github.com>
2026-07-08 22:55:28 -0300

    Update database.sql — 08/07/2026 22:55

--- database.sql
+++ database.sql
@@ -1,3 +1,18 @@
+-- Database: conversaomoedas
+
+-- DROP DATABASE IF EXISTS conversaomoedas;
+
+CREATE DATABASE conversaomoedas
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
 CREATE TABLE IF NOT EXISTS moedas (
     id SERIAL PRIMARY KEY,
     codigo VARCHAR(3) NOT NULL UNIQUE,
